8619f6317026c1f97fc904ee87c0ab48bf07960f
Toshiaki Takahashi <ts-takahashi@nec.com>
2022-03-21 10:57:25 +0000

    Initial commit of Tacker project for O2
    Issue-ID: SMO-52
    Change-Id: Ibbe210b4d3273289929ef345c082f711087fefcf
    Signed-off-by: Toshiaki Takahashi <ts-takahashi@nec.com>

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+from docs_conf import *
+
+branch = 'latest'
+
+linkcheck_ignore = [
+              'http://localhost',
+              'http://localhost.*',
+              'http://127.0.0.1.*',
+              'https://gerrit.o-ran-sc.org.*'
+]
