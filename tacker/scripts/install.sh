44c741aeb8f8fdabdfc05efdb7527445d4cb97b5
satsuu.masayuki <satsuu.masayuki@jp.fujitsu.com>
2022-05-20 01:14:42 +0000

    Added install script about Tacker
    Issue-ID: SMO-65
    Change-Id: Ib5cbbf70d979e28d1fee5ad2935f7a3342181551
    Signed-off-by: Masayuki Satsu <satsuu.masayuki@jp.fujitsu.com>

---
+++ tacker/scripts/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ `whoami` != "stack" ]]
+then
+    echo "You are not stack user."
+    echo https://docs.openstack.org/devstack/latest/#add-stack-user-optional
+    exit 1
+fi
+
+if [ $# != 1 ]; then
+    echo Parameter error: usage: ./install.sh [HOST_IP]
+    exit 1
+fi
+
+git clone https://git.openstack.org/openstack-dev/devstack
+cd devstack
+
+wget https://opendev.org/openstack/tacker/raw/branch/master/devstack/local.conf.standalone
+sed -e "s/127.0.0.1/$1/g" local.conf.standalone > local.conf
+
+./stack.sh
