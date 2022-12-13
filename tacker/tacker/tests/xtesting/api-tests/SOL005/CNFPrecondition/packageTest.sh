20544f294caeb1e85aaa7dcd70324b423cd87795
Koichi Edagawa <edagawa.kc@nec.com>
2022-12-13 13:21:52 +0900

    Fix Xtesting for API conformance based on NFV-TST
    This patch fixes Xtesting code which was provided in the previous
    patch [1].
    
    [1] https://gerrit.o-ran-sc.org/r/c/smo/o2/+/9539/
    
    Issue-ID: SMO-77
    Signed-off-by: Koichi Edagawa <edagawa.kc@nec.com>
    Change-Id: Icef9bd08e460fc6cf43d7bce6e5098c7615bf18f

--- tacker/tacker/tests/xtesting/api-tests/SOL005/CNFPrecondition/packageTest.sh
+++ tacker/tacker/tests/xtesting/api-tests/SOL005/CNFPrecondition/packageTest.sh
@@ -1,43 +1,54 @@
 #!/bin/sh
 
 variableFile=$1
+if [ ! $1 ]; then
+        echo "Input parameter is missing"
+        exit 1
+fi
+
 #curl command for token generation
 curl -X POST -H 'Content-Type:application/json' --data '{"auth": {"scope":
-	{"project": {"domain": {"id": "default"}, "name": "nfv"}}, "identity":
-	{"password": {"user": {"domain": {"id": "default"}, "password":
-	"devstack", "name": "nfv_user"}}, "methods": ["password"]}}}' \
-	-i http://localhost/identity/v3/auth/tokens > outtoken 2>&1
+        {"project": {"domain": {"id": "default"}, "name": "nfv"}}, "identity":
+        {"password": {"user": {"domain": {"id": "default"}, "password":
+        "devstack", "name": "nfv_user"}}, "methods": ["password"]}}}' \
+        -i http://localhost/identity/v3/auth/tokens > outtoken 2>&1
 
 dos2unix outtoken 2>/dev/null
 token=$(cat ./outtoken | grep "X-Subject-Token" | cut -d ' ' -f 2)
+rm -rf outtoken
+if [ ! $token ]; then
+        echo "Token extract is failed , Please check nfv_user exist or not with admin role"
+        exit 1
+fi
 
 #curl command for vnf package create
-curl -g -i -X POST http://localhost:9890/vnfpkgm/v1/vnf_packages \
-	-H "Accept: application/json" -H "Content-Type: application/json" \
-	-H "X-Auth-Token: $token" -d '{}' > outtoken 2>&1
-dos2unix outtoken 2>/dev/null
-
-Packageid=$(cat ./outtoken | grep id | cut -d ' ' -f 2 |sed 's/.$//'|tail -c +2 | head -c -2)
+Packageid=$(curl -g -X POST http://localhost:9890/vnfpkgm/v1/vnf_packages -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Auth-Token: $token" -d '{}' | jq -r .id 2>/dev/null)
 
 echo "$Packageid created"
 #curl command for vnf package upload
 curl -g -i -X PUT http://localhost:9890/vnfpkgm/v1/vnf_packages/$Packageid/package_content \
-	-H "Accept: application/zip" -H "Content-Type: application/zip" \
-	-H "User-Agent: python-tackerclient" -H "X-Auth-Token: $token" --data-binary "@vnfpackage.zip"
+        -H "Accept: application/zip" -H "Content-Type: application/zip" \
+        -H "User-Agent: python-tackerclient" -H "X-Auth-Token: $token" --data-binary "@package_with_helm.zip"
 
-echo "$Packageid uploaded"
+echo "Package is Onboarding..."
+sleep 1m
 
-sleep 1
-curl -g -i -X GET http://localhost:9890/vnfpkgm/v1/vnf_packages/$Packageid \
-	-H "Accept: application/json" -H "User-Agent: python-tackerclient" \
-	-H "X-Auth-Token: $token" > outtoken 2>&1
-dos2unix outtoken 2>/dev/null
+onboardedState=null
+onboardedState=$(curl -g -X GET http://localhost:9890/vnfpkgm/v1/vnf_packages/$Packageid -H "Accept: application/json" -H "User-Agent: python-taerclient" -H "X-Auth-Token: $token" | jq -r .onboardingState)
+echo "$onboardedState onboardinng state"
 
-vnfdid=$(cat ./outtoken | grep vnfdId | cut -d ' ' -f 15 |sed 's/.$//'|tail -c +2 | head -c -2)
+if [ "$onboardedState" = "ONBOARDED" ]; then
+        echo "$Packageid uploaded successully"
+        vnfdid=$(curl -g -X GET http://localhost:9890/vnfpkgm/v1/vnf_packages/$Packageid -H "Accept: application/json" -H "User-Agent: python-taerclient" -H "X-Auth-Token: $token" | jq -r .vnfdId)
 
-rm -rf outtoken
-sed -i '/${vnfPkgId}/d' $variableFile
-sed -i '/${vnfdId}/d' $variableFile
+        sed -i '/${vnfPkgId}/d' $variableFile
+        sed -i '/${vnfdId}/d' $variableFile
 
-echo "\${vnfPkgId}     $Packageid" >> $variableFile # $variableFile environment file path where package ID will be added
-echo "\${vnfdId}     $vnfdid" >> $variableFile
+        echo "\${vnfPkgId}     $Packageid" >> $variableFile # $variableFile environment file path where package ID will be added
+        echo "\${vnfdId}     $vnfdid" >> $variableFile
+        exit 0
+
+else
+        echo "$Packageid upload Failed, please check tacker logs"
+        exit 1
+fi
