86700ee10d722fcc551e07496c71ee329c35d40e
ymarselino <m11202819@gapps.ntust.edu.tw>
2025-08-27 00:08:21 +0800

    Add k8s cluster management function:
    - Add Readme.md under sol003
    - Register k8s cluster
    - Authenticate k8s cluster by upload the base64 kubeconfig <TMP Solution>
    
    sol003 function:
    - Create Descriptor
    - Create Deployment
    - Instantiate Deployment
    - Fix Termination
    - Fix expansion of dot notation for helm values
    
    Change-Id: Ibb7298c482d494107244a5c222a6f0fb6efb1ecd
    Signed-off-by: ymarselino <m11202819@gapps.ntust.edu.tw>

---
+++ nfo/k8s/sol003/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from .models import (
+        NfDeploymentInstance,
+        DeploymentDescriptor,
+        VnfLcmOperation,
+        KubernetesDeployment,
+        KubernetesCluster,
+        )
+
+# Register your models here.
+
+#NfDeploymentInstance,
+@admin.register(NfDeploymentInstance)
+class NfDeploymentInstanceAdmin(admin.ModelAdmin):
+    list_display = ('instance_id','instantiation_state','deployment_namespace','created_at','updated_at')
+    list_filter = ('instantiation_state',)
+    search_fields = ('instance_id','instantiation_state',)
+    #pass
+    # list_display = '__all__'
+    # list_filter = '__all__'
+    # search_fields = '__all__'
+
+#DeploymentDescriptor,
+@admin.register(DeploymentDescriptor)
+class DeploymentDescriptorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'descriptor_id', 'profile_type')
+    #pass
+
+#KubernetesDeployment
+@admin.register(KubernetesDeployment)
+class KubernetesDeploymentAdmin(admin.ModelAdmin):
+    list_display = ('deployment_name','nf_instance','k8s_cluster',)
+    #pass
+
+#KubernetesCluster
+@admin.register(KubernetesCluster)
+class KubernetesClusterAdmin(admin.ModelAdmin):
+    list_display = ('cloud_name','cloud_id','api_endpoint','connection_status',)
+
+@admin.register(VnfLcmOperation)
+class VnfLcmOperationAdmin(admin.ModelAdmin):
+    list_display = ('vnf_instance',
+                    'operation_state',
+                    'operation_type',
+                    'error_details',
+                    'start_time',
+                    )
